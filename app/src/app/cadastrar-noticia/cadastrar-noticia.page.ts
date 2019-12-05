eca822563a0f93b8bd9d94efffef38f2a208491e
Alex Camargo <alexcamargoweb@gmail.com>
2019-12-04 21:31:34 -0300

    validacao

--- app/src/app/cadastrar-noticia/cadastrar-noticia.page.ts
+++ app/src/app/cadastrar-noticia/cadastrar-noticia.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators,  } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-noticia',
