8cd7c56b4c43f28287fe1a5d567a016ae7feeff5
Alex Camargo <alexcamargoweb@gmail.com>
2019-12-04 21:24:41 -0300

    validacao

--- app/src/app/cadastrar-noticia/cadastrar-noticia.page.ts
+++ app/src/app/cadastrar-noticia/cadastrar-noticia.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-noticia',
@@ -8,7 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarNoticiaPage implements OnInit {
 
-  constructor() { }
+  public loginForm: any;
+  messageTitulo = ""
+  messageDescricao = "";
+  messageTipo = "";
+  errorTitulo = false;
+  errorDescricao = false;
+  errorTipo = false;
+
+  constructor(formBuilder: FormBuilder) {
+
+    this.loginForm = formBuilder.group({
+      titulo: ['', Validators.required],
+      descricao: ['', Validators.required],
+      tipo: ['', Validators.required],
+    });
+   }
+
+   login() {
+    let { titulo, descricao, tipo } = this.loginForm.controls;
+ 
+    if (!this.loginForm.valid) {
+      if (!titulo.valid) {
+        this.errorTitulo = true;
+        this.messageTitulo = "Ops! Preencha o título";
+      } else {
+        this.messageTitulo = "";
+      }
+ 
+      if (!descricao.valid) {
+        this.errorDescricao = true;
+        this.messageDescricao = "Ops! Preencha o descrição";
+      } else {
+        this.messageDescricao = "";
+      }
+
+      if (!tipo.valid) {
+        this.errorTipo = true;
+        this.messageTipo = "Ops! Preencha o tipo";
+      } else {
+        this.messageTipo = "";
+      }
+
+    }
+    else {
+      alert("Uhull! Tudo certo");
+    }
+  }   
 
   ngOnInit() {
   }
